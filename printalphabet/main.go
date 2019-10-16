888e2bec3ec0a39c820f1443f623b4d67d648ad7
conci0us <abilda.b@mail.ru>
2019-10-16 17:59:46 +0600

    Completed printalphabet task

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,11 +1,11 @@
-package main 
+package main
 
 import "github.com/01-edu/z01"
 
 func main() {
 
-	for i:='a';<='z; i++{
-		z01.PrintRune(i)
+	for i := 'a'; i <= 'z'; i++ {
+		 z01.PrintRune(i)
 	}
     z01.PrintRune('\n')
 }

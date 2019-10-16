e9103b7abf971bc313946cdcd318a6b4a01b4458
conci0us <abilda.b@mail.ru>
2019-10-16 18:44:48 +0600

    Completed printreversealphabet

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := 'z'; i > 'a'; i-- {
+	for i := 'z'; i >= 'a'; i-- {
 		z01.PrintRune(i)
 	}
-	z01.PrintRune(10)
+	z01.PrintRune(11)
 }

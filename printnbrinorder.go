864bcb1092bf4ab1831cb57ce36f85920dedf2de
conci0us <abilda.b@mail.ru>
2019-10-22 20:41:58 +0600

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -2,45 +2,36 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func intToRune(digit int) rune {
-	return rune(digit + 48)
-}
-
-func splitNumber(number int, arr []int) []int {
-	if number == 0 {
-		return arr
+func size(n int) int {
+	res := 0
+	for ; n > 0; n /= 10 {
+		res++
 	}
-	return splitNumber(number/10, append(arr, number%10))
-}
-
-func customLen(arr []int) int {
-	count := 0
-	for i := range arr {
-		count += 1
-		i++
-	}
-	return count
-}
-
-func customSort(arr []int) []int {
-	for i := 0; i < customLen(arr); i++ {
-		for j := i + 1; j < customLen(arr); j++ {
-			if arr[i] > arr[j] {
-				swap(&arr[i], &arr[j])
-			}
-		}
-	}
-	return arr
+	return res
 }
 
 func PrintNbrInOrder(n int) {
-	if n <= 0 {
+
+	if n == 0 {
 		z01.PrintRune('0')
-	} else {
-		var arr []int
-		arr = customSort(splitNumber(n, arr))
-		for i := 0; i < customLen(arr); i++ {
-			z01.PrintRune(intToRune(arr[i]))
+	}
+
+	var array [19]int
+
+	for i := 0; i <= 18; i++ {
+		array[i] = 20
+	}
+
+	for i := 0; i < size(n); i++ {
+		ten := 1
+		for j := i; j > 0; j-- {
+			ten = ten * 10
 		}
+		array[i] = (n / ten) % 10
+	}
+
+
+	for i := 0; i < size(n); i++ {
+		z01.PrintRune(rune(array[i] + 48))
 	}
 }

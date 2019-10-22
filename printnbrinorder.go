db5232cf2c7dafb131d2540c4dcbba745c580913
conci0us <abilda.b@mail.ru>
2019-10-22 20:51:38 +0600

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -5,22 +5,23 @@ import "github.com/01-edu/z01"
 func size(n int) int {
 	res := 0
 	for ; n > 0; n /= 10 {
-	res++
-}
-return res
+		res++
+	}
+	return res
 }
 
 func PrintNbrInOrder(n int) {
-	
+
 	if n == 0 {
 		z01.PrintRune('0')
 	}
+
 	var array [19]int
-	
+
 	for i := 0; i <= 18; i++ {
 		array[i] = 20
 	}
-	
+
 	for i := 0; i < size(n); i++ {
 		ten := 1
 		for j := i; j > 0; j-- {
@@ -28,6 +29,8 @@ func PrintNbrInOrder(n int) {
 		}
 		array[i] = (n / ten) % 10
 	}
+
+	SortIntegerTable(array[:])
 	
 	for i := 0; i < size(n); i++ {
 		z01.PrintRune(rune(array[i] + 48))

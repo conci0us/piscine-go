cca7b1408f508a87cfb01d702778f505a7cffb91
conci0us <abilda.b@mail.ru>
2019-10-22 20:58:26 +0600

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -29,8 +29,7 @@ func PrintNbrInOrder(n int) {
 		}
 		array[i] = (n / ten) % 10
 	}
-
-	func PrintNbrInOrder(n int)
+	SortIntegerTable(array[:])
 	for i := 0; i < size(n); i++ {
 		z01.PrintRune(rune(array[i] + 48))
 	}

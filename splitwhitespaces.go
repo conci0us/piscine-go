064e846fb542b870eea91ffb26262caeb604a5a3
conci0us <abilda.b@mail.ru>
2019-10-24 20:09:21 +0600

    splitwhitespaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,8 +1,29 @@
 package piscine
 
-import "strings"
-
 func SplitWhiteSpaces(str string) []string {
-	tab := strings.Fields(str)
-	return tab
+	
+	TextToString := ""
+	t := []string{}
+	for i, v := range str {
+		if i == lent2(str)-1 && string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+			TextToString += string(v)
+			t = append(t, TextToString)
+		}else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+				TextToString += string(v)
+		}else{
+			if len(TextToString)>=1{
+				t = append(t, TextToString)
+			}
+			TextToString = ""
+		}
+	}
+	return t
 }
+func lent2(d string ) int{
+	inc:=0
+		for _, _= range d{
+			inc ++
+		}
+		 return inc
+	}
+	

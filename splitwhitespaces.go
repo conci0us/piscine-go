91fc4387c03b7d2f069e4fb9015f3155222b17cc
conci0us <abilda.b@mail.ru>
2019-10-24 20:31:08 +0600

    splitwhitespaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,29 +1,39 @@
 package piscine
 
 func SplitWhiteSpaces(str string) []string {
-	
-	    TextToString := ""
-	    t := []string{}
-	    for i, v := range str {
-		        if i == lent2(str)-1 && string(v) != " " && string(v) != "\t" && string(v) != "\n" {
-			            TextToString += string(v)
-			            t = append(t, TextToString)
-		        }else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
-			                    TextToString += string(v)
-		        }else{
-			            if len(TextToString)>=1{
-			                    t = append(t, TextToString)
-			            }
-			            TextToString = ""
-		        }
-	    }
-	    return t
-}
-func lent2(d string ) int{
-	    inc:=0
-		        for _, _= range d{
-		                inc ++
-		        }
-
-		        return inc
+	var len = 0
+	var strlLen = 0
+	var start = 0
+	var j = 0
+	var wasLetter = false
+	for _, v := range str {
+		if (v == '\n' || v == '\t' || v == ' ') && wasLetter {
+			len++
+			wasLetter = false
+		} else if v != '\n' && v != '\t' && v != ' ' {
+			wasLetter = true
+		}
+		strlLen++
+	}
+	if wasLetter {
+		len++
+		wasLetter = false
+	}
+	arr := make([]string, len)
+	for i, v := range str {
+		if (v == '\n' || v == '\t' || v == ' ') && wasLetter {
+			arr[j] = str[start:i]
+			j++
+			wasLetter = false
+		} else if v != '\n' && v != '\t' && v != ' ' {
+			if !wasLetter {
+				start = i
+			}
+			wasLetter = true
+		}
+	}
+	if wasLetter {
+		arr[j] = str[start:strlLen]
+	}
+	return arr
 }
